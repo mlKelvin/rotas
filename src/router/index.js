c1fb2472c4053f3ecaa5a888a0e84cd6731b08ae
KELVIN MELIM <kelvin.melim@alunos.sc.senac.br>
2022-09-30 20:43:29 -0300

    1

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/Marca',
+    name: 'Marca',
+
+    component: () => import('../views/MarcaView.vue')
+  },
+  {
+    path: '/marca-form/:id',
+    name: 'marca-form',
+
+    component: () => import('../views/MarcaFormView.vue')
+  },
+  {
+    path: '/Carro',
+    name: 'Carro',
+
+    component: () => import('../views/CarroView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/MarcaView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1>Marca</h1>
+
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th></th>
+            </tr>
+            <tr v-for="m in marcas" :key="m.Id">
+                <th>{{m.Id}}</th>
+                <th>{{m.nome}}</th>
+                <th> <a href="#" @click="editar(m)">Editar</a> </th>
+            </tr>
+        </table>
+
+    </div>
+
+</template>
+
+
+<script>
+
+export default {
+    data() {
+        return {
+            marcas: [
+                {
+                    id:1,
+                    nome: 'Hyundai'
+                },
+                {
+                    id:2,
+                    nome: 'Ford'
+                },
+            ]
+        }
+    },
+    methods: {
+        editar(marca){
+            this.$router.push(`/marca-form/${marca.id}`)
+        }
+    }
+}
+</script>
